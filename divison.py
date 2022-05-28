9ebf69a4376164fad2477844c21740dcff1ebb7a
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-05-28 14:18:22 +0530

    Create divison.py

---
+++ divison.py
@@ -0,0 +1,12 @@
+## Task 
+##The provided code stub reads two integers,  and , from STDIN.
+##Add logic to print two lines. 
+##The first line should contain the result of integer division,  // . 
+##The second line should contain the result of float division,  / .
+
+a = int(input())
+b = int(input())
+result1 = a//b
+result2 = a/b
+print(result1)
+print(result2)
