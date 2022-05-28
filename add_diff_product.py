0f0c206a8848cc50d21ff3595339560d217ab3c9
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-05-28 14:14:16 +0530

    Create add_diff_product.py

---
+++ add_diff_product.py
@@ -0,0 +1,14 @@
+##Task 
+##The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+##The first line contains the sum of the two numbers.
+##The second line contains the difference of the two numbers (first - second).
+##The third line contains the product of the two numbers.  
+  
+  a = int(input())
+  b = int(input())
+  result1 = a+b
+  result2 = a-b
+  result3 = a*b
+  print(result1)
+  print(result2)
+  print(result3)
