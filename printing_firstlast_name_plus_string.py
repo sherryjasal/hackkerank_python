4936937b4bb879ea766745a1fc33e631e53e9feb
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-05-29 07:18:29 +0530

    Create printing_firstlast_name_plus_string.py

---
+++ printing_firstlast_name_plus_string.py
@@ -0,0 +1,9 @@
+## Complete the print_full_name function in the editor below.
+##print_full_name has the following parameters:
+##string first: the first name
+##string last: the last name
+
+def print_full_name(first, last):
+    string_first = first
+    string_last = last
+    print('Hello ' + string_first + ' ' + string_last + '! You just delved into python.')
