c05b3c6d75542b5909df5422641e961afa58a988
RKaur <32834083+sherryjasal@users.noreply.github.com>
2022-08-07 15:43:27 +0530

    Create triangle_quest.py

---
+++ triangle_quest.py
@@ -0,0 +1,10 @@
+##You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
+##1
+##22
+##333
+##4444
+##55555
+
+
+for i in range(1,int(input())): 
+    print (i*10**i//9)
